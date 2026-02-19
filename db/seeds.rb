dc5110954f30db2c3abd8319aebd7c3319f532d2
Ivan Xue <ivanxue152@gmail.com>
2026-02-19 21:08:54 +1100

    added last name to schema and seeded 2 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  first_name: "Sports",
+  last_name: "Buddy",
+  email: "sports@test.com",
+  password: "secret",
+  password_confirmation: "secret"
+)
+
+User.create!(
+  first_name: "Test",
+  last_name: "Test",
+  email: "test@test.com",
+  password: "secret",
+  password_confirmation: "secret"
+)
